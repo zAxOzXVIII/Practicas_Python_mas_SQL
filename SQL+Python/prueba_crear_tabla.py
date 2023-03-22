7e302530858592347a9c7f5266ccae4341c9772f
zAxOzXVIII <119118178+zAxOzXVIII@users.noreply.github.com>
2023-03-22 15:01:42 -0400

    Finalizada 1
    se importo la libreria pymysql, y se hizo una conexion para poder crear una base de datos, luego mediante condiciones se genera otra conexion a la db creada para crear una tabla llamada persona

--- SQL+Python/prueba_crear_tabla.py
+++ SQL+Python/prueba_crear_tabla.py
@@ -1,1 +1,42 @@
-#zAxOz 
+# Importamos pymysql
+import pymysql
+
+conexion= pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    # database="",
+    # port=""
+    
+)
+
+query = conexion.cursor()
+
+sql = "CREATE DATABASE IF NOT EXISTS prueba1_py"
+# try:
+if(query.execute(sql)):
+    print("Se ha creado una base de datos")
+else:
+    conexion.close
+    conexion = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="prueba1_py"
+    )
+    query = conexion.cursor()
+# except ProgrammingError as e:
+# print("Error: "+e)
+
+sql2 = """CREATE TABLE IF NOT EXISTS persona(
+    id_p INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(48) NOT NULL,
+    cedula VARCHAR(9) NOT NULL,
+    fecha_nacimiento DATE,
+    hobbie VARCHAR(64),
+    PRIMARY KEY(id_p)
+    )"""
+query.execute(sql2)
+
+conexion.commit()
+conexion.close()
